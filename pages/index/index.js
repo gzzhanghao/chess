7778b6834c07dc74f223f566035b15242af75b07
Jason <jason.daurus@gmail.com>
2016-09-30 23:42:41 +0800

    basic logic

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,34 @@
 Page({
 
+  black: '♜♞♝♛♚♟',
+  white: '♖♘♗♕♔♙',
+
   data: {
     chessboard: '♖♘♗♕♔♗♘♖♙♙♙♙♙♙♙♙                                ♟♟♟♟♟♟♟♟♜♞♝♛♚♝♞♜',
+    cellState: {},
+    selected: null,
+  },
+
+  onTapCell(event) {
+    const dataset = event.currentTarget.dataset
+
+    const row = dataset.row|0
+    const col = dataset.col|0
+
+    if (this.data.selected) {
+      // @todo check movement
+      return this.setData({ cellState: {} })
+    }
+
+    const cellState = { [`${row} ${col}`]: 'selected' }
+
+    const target = this.data.chessboard[8 * row + col]
+    // @todo update `available` status
+    // @todo update `attack` status
+
+    // @todo update `danger` status
+
+    this.setData({ cellState })
   },
 
 })
